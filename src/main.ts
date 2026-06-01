997d709cdd9f8d03789f74021ed48649a089f62f
Sidartha Kumar <sidarthakumar@gmail.com>
2026-06-01 16:40:33 +0530

    fix: resolve compression and tsconfig module issues

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import { ValidationPipe, Logger } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 import helmet from 'helmet';
-import * as compression from 'compression';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './common/filters/http-exception.filter';
 import { LoggingInterceptor } from './common/interceptors/audit.interceptor';
 
 async function bootstrap() {
+  const compression = require('compression');
   const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
 
@@ -19,7 +19,7 @@ async function bootstrap() {
 
   // Security
   app.use(helmet());
-  app.use(compression());
+  app.use((compression as any)());
 
   // CORS
   app.enableCors({
