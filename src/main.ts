fe3e02bd9e9869095bbb78a11079e5c51ca14743
Sidartha Kumar <sidarthakumar@gmail.com>
2026-06-01 11:15:29 +0530

    fix: use commonjs module system for NestJS compatibility

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { ValidationPipe, Logger } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 import helmet from 'helmet';
-import * as compression from 'compression';
+import compression from 'compression';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './common/filters/http-exception.filter';
 import { LoggingInterceptor } from './common/interceptors/audit.interceptor';
