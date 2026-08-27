8a7683a9910889815b0dd2cc17b3a94caf6f2bad
Sidartha Kumar <sidarthakumar@gmail.com>
2026-08-27 23:03:26 +0530

    Sidebar: remove standalone Vehicle Log entry (merged into Visitors page as a sub-tab), rename Visitors entry to 'Visitors & Vehicles'

---
+++ 027_merge_visitors_vehicles_sidebar.sql
@@ -0,0 +1,2 @@
+UPDATE ui_control_elements SET is_active = false WHERE key = 'sidebar.gate.vehicles';
+UPDATE ui_control_elements SET label = 'Visitors & Vehicles' WHERE key = 'sidebar.gate.visitors';
